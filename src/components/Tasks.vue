3cafaccaa68a15cc0cb40dfe9fbba1d338dd7845
Peter <youngeunpc@yahoo.com>
2025-03-11 22:53:17 -0300

    Toggle reminder functionality

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,7 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
         <Task 
+            @toggle-reminder="$emit('toggle-reminder', task.id)"
             @delete-task="$emit('delete-task', task.id)" 
             :task="task" 
         />
@@ -18,6 +19,6 @@ export default {
     components: {
         Task,
     },
-    emits: ['delete-task'],
+    emits: ['delete-task', 'toggle-reminder'],
 };
 </script>
