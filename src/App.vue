681d7e419dd6f0733b04827d4648b673c8e068f9
Peter <youngeunpc@yahoo.com>
2025-03-11 21:23:16 -0300

    Implemented tasks and task vues

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="container">
-    <Header />
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
   },
-}
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+      }
+    ];
+  },
+};
 </script>
 
 <style>
