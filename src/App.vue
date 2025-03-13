bf8cf94ec36a2d3db2a5f2f2e1d89788f282585b
Peter <youngeunpc@yahoo.com>
2025-03-13 19:04:31 -0300

    Started production and added a fetch request to backend

--- src/App.vue
+++ src/App.vue
@@ -48,29 +48,15 @@ export default {
     },
     toggleReminder(id) {
       this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task)
+    },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks');
+      const data = await res.json();
+      return data;
     }
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-      }
-    ];
+  async created() {
+    this.tasks = await this.fetchTasks();
   },
 };
 </script>
